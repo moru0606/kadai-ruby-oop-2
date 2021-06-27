764959f19f6a26b8fb28c299a8e5a8cbfe066ae1
hiroaki.ooki <moru150606@gmail.com>
2021-06-27 08:32:28 +0000

    kadai-ruby-oop-2

--- mentor.rb
+++ mentor.rb
@@ -4,17 +4,13 @@ class Mentor
       self.name = name
     end 
   def job
-    p "#{self.name}です。私は現役ITプロフェッショナルです。"
+    puts "#{self.name}です。私は現役ITプロフェッショナルです。"
   end
 end
 class RailsMentor < Mentor
-  attr_accessor :name
-   def initialize(name)
-     self.name = name
-     super
-   end 
+  
   def job
-    p "#{self.name}です。私はRubyとRailsでWebアプリケーションを作ります。"
+    puts "#{self.name}です。私はRubyとRailsでWebアプリケーションを作ります。"
   end
 end
 kirameki = Mentor.new("煌木")
